bb5f3ca5cd58821c7f73c19fb5598eaefdc8e9b0
rafflores17-prog <rafflores17@gmail.com>
2026-04-13 07:38:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,27 @@
+// === SEU CÓDIGO DO PWA (Cache e Instalação) ===
+const CACHE_NAME = 'bgdv-links-v1';
+const assets = ['/', '/index.html', '/style.css', '/script.js'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// === CÓDIGO DA MONETAG (Adicionado no final) ===
 self.options = {
     "domain": "5gvci.com",
     "zoneId": 10868948
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+};
+self.lary = "";
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
