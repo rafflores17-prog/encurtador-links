d96a4afc18fc8a292cdbdcfc0ea09cbbdec8ee74
rafflores17-prog <rafflores17@gmail.com>
2026-04-17 18:38:28 -0300

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,40 +1,21 @@
-const CACHE_NAME = "bgdv-v3";
+const CACHE_NAME = "bgdv-v5";
 
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js"
-];
-
-self.addEventListener("install", (e) => {
+self.addEventListener("install", e => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
 });
 
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => key !== CACHE_NAME && caches.delete(key))
-      )
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
-  self.clients.claim();
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e => {
   if (e.request.url.includes("/encurtar")) return;
 
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
